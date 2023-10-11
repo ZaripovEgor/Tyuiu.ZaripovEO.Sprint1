c6b36d03e5b5885ee6ca13220aec3301dd03a016
ZaripovEgor <144524848+ZaripovEgor@users.noreply.github.com>
2023-10-11 18:41:37 +0500

    Task1

---
+++ Tyuiu.ZaripovEO.Sprint1.Task1.V12.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ZaripovEO.Sprint1.Task1.V12.Lib
+{
+    public class DataService : ISprint1Task1V12
+    {
+        public double Calculate(double x, double y)
+        {
+            return (y + x) / 6;
+        }
+    }
+}
---
+++ Tyuiu.ZaripovEO.Sprint1.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZaripovEO.Sprint1.Task1.V12.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint1.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double y = 2.0;
+            double x = 4.0;
+
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
